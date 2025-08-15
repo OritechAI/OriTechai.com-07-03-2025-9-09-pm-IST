768cf3bec752eb4878a0310a43d76c649a0df690
OritechAI <selenica3@gmail.com>
2025-08-15 00:10:22 -0500

    Create lean React + Vite landing page

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    }
-  }
+  plugins: [react()]
 });
